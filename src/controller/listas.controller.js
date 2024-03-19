1070765bbe30a6ca077a9f891b378b07ae82c339
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-19 20:38:30 -0300

    añadimos la funcion de editar color

--- src/controller/listas.controller.js
+++ src/controller/listas.controller.js
@@ -12,7 +12,7 @@ export const listarColores = async (req, res) => {
     }
 };
 
-export const borrarTarea = async (req, res) => {
+export const borrarColor = async (req, res) => {
     try {
       const buscarColor = await Color.findById(req.params.id);
       if (!buscarColor) {
@@ -45,3 +45,19 @@ export const crearColor = async (req, res) => {
       });
     }
 }; 
+
+export const editarColor = async(req, res) => {
+    try{
+        const buscarColor = await Color.findById(req.params.id)
+        if(!buscarColor){
+            return res.status(404).json({mensaje: "no se pudo editar el color, el id es incorrecto"})
+        }
+        await Color.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({mensage: "el color fue modificado exitosamente"})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            mensaje: "ocurrio un error al querer intentar editar el color"
+        })
+    }
+}
--- src/routes/listas.routes.js
+++ src/routes/listas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import { borrarColor, crearColor, listarColores } from "../controller/listas.controller";
 
 const Coloresrouter = Router();
 
-Coloresrouter.route('/tareas').get().post();
-Coloresrouter.route('/tarea/:id').delete();
+Coloresrouter.route('/tareas').get(listarColores).post(crearColor);
+Coloresrouter.route('/tarea/:id').delete(borrarColor);
 
 export default Coloresrouter
